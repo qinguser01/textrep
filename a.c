d0ae30f2a2a53c912b1892ea0cb16559ef2a9e2f
qinguser01 <94903590+qinguser01@users.noreply.github.com>
2021-12-29 00:51:45 +0800

    Add files via upload

--- a.c
+++ a.c
@@ -1,14 +1,44 @@
-#include<stdio.h>
+#include"pcpy.h"
 
 
-
-
-
-
-int main(void)
+int procreate(const char * SFILE ,const char * DFILE,int pronum,int blocksize)
 {
-	int a=0;
-	printf("wangqingdashab%d\n",a);
-	return 0;
+	int flag;
+	pid_t pid;
 
+	int pos;
+	char cblock[10];
+	char cpos[10];
+	bzero(cblock,sizeof(cblock));
+	bzero(cpos,sizeof(cblock));
+	for(flag=0;flag<pronum;flag++)
+	{
+		pid=fork();
+		if(pid==0)
+			break;
+	}
+	if(pid>0)
+	{
+		printf("parent pid[%d] ...\n",getpid());
+		pid_t zpid;
+		while((zpid=waitpid(-1,NULL,WNOHANG))!=-1)
+		{
+			if(zpid>0)
+			{
+				printf("parent wait success[%d]\n",zpid);
+			}
+		}
+	}
+	else if(pid==0)
+	{
+		pos=flag*blocksize;
+		sprintf(cblock,"%d",blocksize);
+		sprintf(cpos,"%d",pos);
+		execl("/home/studycolin/0322/1228/COpy","./COpy",SFILE,DFILE,cblock,cpos,NULL);
+	}else
+	{
+		perror("fork call failed");
+		exit(-1);
+	}
+	return 0;
 }
